545ed3ecbf69e1f1edd05d20456d513c936c6dbe
ashutoshpandey28 <ashutoshpandey.developer@gmail.com>
2021-06-29 10:05:17 +0530

    Initial Commit

---
+++ JobFinder App/JobFinder_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobFinder_AppApp.swift
+//  JobFinder App
+//
+//  Created by ashutosh on 29/06/21.
+//
+
+import SwiftUI
+
+@main
+struct JobFinder_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
